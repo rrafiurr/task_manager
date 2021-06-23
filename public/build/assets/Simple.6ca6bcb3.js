5e5d7844f27b19ca892673ba016360159f75f5b8
rrafiurr <rrafiurr@gmail.com>
2021-06-24 02:20:10 +0600

    again build files

---
+++ public/build/assets/Simple.6ca6bcb3.js
@@ -0,0 +1,1 @@
+import{r,o as e,c as o,g as s,x as n}from"./vendor.9281c9b8.js";const t={},a=n("simple ");t.render=function(n,t){const c=r("router-view");return e(),o("div",null,[a,s(c)])};export default t;
